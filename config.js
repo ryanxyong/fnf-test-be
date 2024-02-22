ed71e36c95921ea06af3c3c6488194c937aec3ea
Ryan Yong <ryan.yong.24@dartmouth.edu>
2024-02-22 13:44:57 -0500

    daf

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-export const PORT = 5555;
+export const PORT = 8080;
 
 export const mongoDBURL = 'mongodb+srv://cs98workout:bigchungus@master.lvimwe1.mongodb.net/appDB?retryWrites=true&w=majority';
